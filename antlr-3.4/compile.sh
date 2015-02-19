9eef29afcf80c0b0ea3e18976e35633d6e07dfe7
ben <types@ccs.neu.edu>
2015-02-18 23:35:04 -0500

    [antlr] removed unused files

--- antlr-3.4/compile.sh
+++ antlr-3.4/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-javac -d ./runtime/Java/target/classes -classpath ./lib/stringtemplate-3.2.1.jar @COMPILE_ARGS
+javac -d ./target/classes -classpath ./lib/stringtemplate-3.2.1.jar @COMPILE_ARGS
